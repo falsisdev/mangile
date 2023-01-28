7d2730262703a592235379690373865115efbb40
Falsis <keremtokul@hotmail.com>
2023-01-28 18:16:11 +0300

    feat: added github issues to /contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -15,8 +15,9 @@
     <article class="prose max-w-none">
         <h2>Discord Sunucusu</h2>
     </article><br/>
+    <div class="flex flex-row flex-wrap">
         <div class="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://cdn.discordapp.com/attachments/775822548519616562/1068887391051460628/kremlinbanner1.2.jpg" /></figure>
+        <figure><img src="https://cdn.discordapp.com/attachments/775822548519616562/1068883023963107379/kremlinbanner1.1.jpg" /></figure>
         <div class="card-body">
             <h2 class="card-title">        
                 <div class="avatars absolute pb-20">
@@ -27,8 +28,25 @@
             </h2><br/>
             <p>Mangile destek için kullanılan ve aslında geliştiricilere destek için açılmış Kremlin Geliştirici topluluğu Discord sunucusuna aşağıdaki buton aracılığıyla katılabilirsiniz.</p>
             <div class="card-actions justify-end">
-            <button class="btn btn-primary"><span class="pr-2"><font-awesome-icon icon="fa-solid fa-right-to-bracket" /></span>Katıl</button>
+            <a href="https://discord.gg/BDXkSgVaXe"><button class="btn btn-primary"><span class="pr-2"><font-awesome-icon icon="fa-solid fa-right-to-bracket" /></span>Katıl</button></a>
             </div>
+        </div>
+        </div>
+        <div class="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src="https://cdn.discordapp.com/attachments/775822548519616562/1068908750129348668/image.png" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">        
+                <div class="avatars absolute pb-20">
+                <div class="w-24 mask mask-squircl">
+                    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" />
+                </div>
+                </div>
+            </h2><br/>
+            <p>Eğer Mangile websitesinde bazı sorunlar yaşıyorsanız bize en hızlı ulaşabileceğiniz yer olan Github Issues bölümüne aşağıdaki butona tıklayarak ulaşabilirsiniz.</p>
+            <div class="card-actions justify-end">
+            <a href="https://github.com/falsisdev/mangile/issues"><button class="btn btn-primary"><span class="pr-2"><font-awesome-icon icon="fa-solid fa-link" /></span>Github</button></a>
+            </div>
+        </div>
         </div>
         </div><br/>
     <article class="prose max-w-none">
