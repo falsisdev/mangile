b98bd7c12e3ee9d8edffa7e195f4e54f9501299d
Falsis <keremtokul@hotmail.com>
2024-09-16 20:11:12 +0300

    perf(version): nuxt compatibilityVersion upgraded to 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   modules: [
     "@vueuse/nuxt",
     "nuxt-swiper",
