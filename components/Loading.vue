5970ccfe838ce48b63263c293783e4cb1680bf44
falsisdev <keremtokul@hotmail.com>
2026-06-21 22:57:54 +0300

    chore(loading): remove unused Icon import and update loading information

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { Icon } from '@iconify/vue';
-
 const props = defineProps({
     what: String,
     type: String
@@ -8,18 +6,16 @@ const props = defineProps({
 
 const infos = [
     "Mangile geliştirilmeye 2022'nin üçüncü çeyreğinde başlanmıştır.",
-    "Mangile geliştirilmeye başlandığında tek geliştirici Falsis henüz 15 yaşında bir 9. sınıf öğrencisiydi.",
+    "Mangile geliştirilmeye başlandığında tek geliştirici Falsis henüz 15 yaşında bir 9. sınıf öğrencisiydi. Şu sıralar üniversitede.",
     "Mangile'ın adı Manga ve İngilizce timsah anlamına gelen crocodile'ın birleşmesiyle oluşmuştur.",
     "Mangile'ın geliştirilmesine ilham veren web sitesi MangaDex'tir.",
     "Falsis, Mangile'ı geliştirmeden önce Ani+ adında bir anime sitesi denemesi yapıp vazgeçmişti.",
-    "Mangile geliştirilmeye başlandığında, Nuxt 3. sürümü henüz erken erişimde bile değildi.",
     "Mangile geliştirilmeye başlandığında 2022 Dünya Kupası başlamamıştı.",
     "Mangile geliştirilmeye başlandığında 1 Dolar 18 Liraydı.",
-    "Mangile'ın planlanan stabil sürüm yayınlanma tarihi, geliştirilmeye başlandığı tarihten daha uzak.",
     "Mangile geliştirilmeye başlandığında 2023 genel seçimlerine 8.5 ay vardı.",
     "Mangile geliştirme sürecinde 4 kere büyük yazılım değişikliğine gitmiştir.",
     "Mangile geliştirilmeye başlandığında Attack On Titan animesi henüz final vermemişti.",
-    "Mangile geliştirilmeye başlandığında Chainsaw Man animesi yayınlanmaya yeni başlamıştı.",
+    "Mangile geliştirilmeye başlandığında Chainsaw Man animesinin ilk sezonu yayınlanmaya henüz başlamıştı.",
     "Ekibimiz tek kişiliktir."
 ]
 </script>
